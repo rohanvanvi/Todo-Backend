a210a5914954117965d34d8d3433e9ab3d3edac8
rohanvanvi <rohanvanvi>
2025-05-19 03:55:05 +0530

    error fix

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -9,7 +9,13 @@ import { UnauthorizedException } from "../utils/appError";
 
 export const googleLoginCallback = asyncHandler(
   async (req: Request, res: Response) => {
-    const currentWorkspace = req.user?.currentWorkspace;
+    if (!req.user) {
+      return res.redirect(
+        `${config.FRONTEND_GOOGLE_CALLBACK_URL}?status=failure`
+      );
+    }
+
+    const currentWorkspace = req.user.currentWorkspace;
 
     if (!currentWorkspace) {
       return res.redirect(
@@ -18,7 +24,7 @@ export const googleLoginCallback = asyncHandler(
     }
 
     // Generate token for Google login
-    const token = jwt.sign({ id: req.user?._id }, config.JWT_SECRET, {
+    const token = jwt.sign({ id: req.user._id }, config.JWT_SECRET, {
       expiresIn: '24h'
     });
 
@@ -36,7 +42,7 @@ export const registerUserController = asyncHandler(
 
     const user = await registerUserService(body);
     
-    const token = jwt.sign({ id: user._id }, config.JWT_SECRET, {
+    const token = jwt.sign({ id: user.userId }, config.JWT_SECRET, {
       expiresIn: '24h'
     });
 
